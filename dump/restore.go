8853d89f3dfda8455a76e73384de5d856c51867e
mishamyrt <misha@myrt.co>
2024-03-01 00:20:49 +0300

    fix: use original state write order

--- dump/restore.go
+++ dump/restore.go
@@ -8,39 +8,23 @@ import (
 
 // Restore device state
 func Restore(h hid.Handler, s *State) error {
-	err := restoreLights(h, s.Lights)
+	k := keys.New(h, nil)
+	l := light.New(h)
+	colors := light.ParseColorsState(s.Lights.Colors)
+	effects := light.ParseParamsState(s.Lights.Params)
+	err := l.SetBacklightColors(colors)
 	if err != nil {
 		return err
 	}
-	err = restoreKeys(h, s.Keys)
+	err = k.SetMacCodes(s.Keys.Mac)
 	if err != nil {
 		return err
 	}
-	return nil
-}
-
-func restoreLights(h hid.Handler, s light.State) error {
-	f := light.New(h)
-	colors := light.ParseColorsState(s.Colors)
-	effects := light.ParseParamsState(s.Params)
-	err := f.SetEffects(effects)
+	err = k.SetWinCodes(s.Keys.Win)
 	if err != nil {
 		return err
 	}
-	err = f.SetBacklightColors(colors)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func restoreKeys(h hid.Handler, s keys.State) error {
-	f := keys.New(h, nil)
-	err := f.SetMacCodes(s.Mac)
-	if err != nil {
-		return err
-	}
-	err = f.SetWinCodes(s.Win)
+	err = l.SetEffects(effects)
 	if err != nil {
 		return err
 	}
