4fa5dbd4682358d1ca8a131a0ad25943fc4a058d
mishamyrt <misha@myrt.co>
2024-05-05 00:27:10 +0300

    test: add missing short code tests

--- features/keys/short.go
+++ features/keys/short.go
@@ -3,17 +3,17 @@ package keys
 import "github.com/mishamyrt/nuga-lib/layout"
 
 // FromShortKeyCode converts short code to full code
-func FromShortKeyCode(code byte) uint32 {
+func FromShortKeyCode(code uint8) uint32 {
 	return uint32(code) << 24
 }
 
 // ToShortKeyCode converts full code to short code
-func ToShortKeyCode(code uint32) byte {
+func ToShortKeyCode(code uint32) uint8 {
 	return byte(code >> 24)
 }
 
 // FindKeyNameByShortCode finds key name by short code
-func FindKeyNameByShortCode(code byte) layout.KeyName {
+func FindKeyNameByShortCode(code uint8) layout.KeyName {
 	for k, v := range layout.Keys {
 		if ToShortKeyCode(v.Code) == code {
 			return k
@@ -23,7 +23,7 @@ func FindKeyNameByShortCode(code byte) layout.KeyName {
 }
 
 // FindShortKeyCode finds key short code by name
-func FindShortKeyCode(name layout.KeyName) byte {
+func FindShortKeyCode(name layout.KeyName) uint8 {
 	code := layout.FindKeyCode(name)
 	return ToShortKeyCode(code)
 }
--- features/keys/short_test.go
+++ features/keys/short_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/mishamyrt/nuga-lib/features/keys"
+	"github.com/mishamyrt/nuga-lib/layout"
 )
 
 func TestFromShortKeyCode(t *testing.T) {
@@ -44,6 +45,47 @@ func TestToShortKeyCode(t *testing.T) {
 			result := keys.ToShortKeyCode(tt.code)
 			if result != tt.expected {
 				t.Errorf("ToShortKeyCode(%#x): expected %#x, got %#x", tt.code, tt.expected, result)
+			}
+		})
+	}
+}
+
+func TestFindKeyNameByShortCode(t *testing.T) {
+	tests := []struct {
+		code     byte
+		expected layout.KeyName
+	}{
+		{0x10, layout.KeyM},
+		{0x1C, layout.KeyY},
+		{0x15, layout.KeyR},
+		{0x17, layout.KeyT},
+		{0x99, layout.KeyNone},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%#x", tt.code), func(t *testing.T) {
+			result := keys.FindKeyNameByShortCode(tt.code)
+			if result != tt.expected {
+				t.Errorf("FindKeyNameByShortCode(%#x): expected %s, got %s", tt.code, tt.expected, result)
+			}
+		})
+	}
+}
+
+func TestFindShortKeyCode(t *testing.T) {
+	tests := []struct {
+		name     layout.KeyName
+		expected byte
+	}{
+		{layout.KeyM, 0x10},
+		{layout.KeyY, 0x1C},
+		{layout.KeyR, 0x15},
+		{layout.KeyT, 0x17},
+	}
+	for _, tt := range tests {
+		t.Run(string(tt.name), func(t *testing.T) {
+			result := keys.FindShortKeyCode(tt.name)
+			if result != tt.expected {
+				t.Errorf("FindShortKeyCode(%s): expected %#x, got %#x", tt.name, tt.expected, result)
 			}
 		})
 	}
