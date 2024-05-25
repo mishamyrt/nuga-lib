fa6323a0aeca74437f08faf16fcabe77fe1c49e5
mishamyrt <misha@myrt.co>
2024-05-25 20:55:43 +0300

    feat: allow variable length codes slice

--- features/keys/keycode.go
+++ features/keys/keycode.go
@@ -2,9 +2,10 @@ package keys
 
 // PackKeyCodes packs key codes from raw keys payload
 func PackKeyCodes(p []byte) []uint32 {
-	values := make([]uint32, 256)
+	count := len(p) / 4
+	values := make([]uint32, count)
 	var offset int
-	for i := 0; i < 256; i++ {
+	for i := 0; i < count; i++ {
 		for j := 0; j < 4; j++ {
 			offset = (i * 4) + (3 - j)
 			values[i] = (values[i] << 8) | uint32(p[offset])
@@ -15,9 +16,9 @@ func PackKeyCodes(p []byte) []uint32 {
 
 // UnpackKeyCodes unpacks key codes to raw keys payload
 func UnpackKeyCodes(v []uint32) []byte {
-	payload := make([]byte, 1024)
+	payload := make([]byte, len(v)*4)
 	var offset int
-	for i := 0; i < 256; i++ {
+	for i := 0; i < len(v); i++ {
 		offset = (i * 4)
 		payload[offset] = byte(v[i] & 0xFF)
 		payload[offset+1] = byte((v[i] >> 8) & 0xFF)
