86586ae2d6c06a6264a07a7aa0b4685e0cd21ac0
mishamyrt <misha@myrt.co>
2024-05-25 21:43:58 +0300

    feat: update simulation

--- simulation.go
+++ simulation.go
@@ -1,26 +1,19 @@
 package nuga
 
 import (
-	"github.com/mishamyrt/nuga-lib/device"
+	"github.com/mishamyrt/nuga-lib/dump"
 	"github.com/mishamyrt/nuga-lib/features"
 )
 
 const fakePath = "/simulated/device/path"
 
-// SimulationTemplate represents template for simulated keyboard
-type SimulationTemplate struct {
-	Name     device.Model        `json:"name"`
-	Firmware string              `json:"firmware"`
-	Data     *features.StateData `json:"data"`
-}
-
 // FromTemplate creates simulated keyboard
-func FromTemplate(t *SimulationTemplate) (*Device, error) {
+func FromTemplate(t *dump.State) (*Device, error) {
 	return &Device{
-		Name:         t.Name,
+		Name:         t.Model,
 		Path:         fakePath,
 		Firmware:     t.Firmware,
-		Features:     features.NewSimulation(t.Data, t.Name),
-		Capabilities: GetCapabilities(t.Name),
+		Features:     features.NewSimulation(&t.Data, t.Model),
+		Capabilities: GetCapabilities(t.Model),
 	}, nil
 }
